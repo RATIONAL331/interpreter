82de0df1cbbc2fdfb4985f44db90a03f4751fefa
김령진/서버개발팀/ED <ryungjin.kim@nhnedu.com>
2022-01-22 23:33:09 +0900

    [Chapter 2] 기본적인 let 파싱하기

---
+++ parser/parser_test.go
@@ -0,0 +1,62 @@
+package parser
+
+import (
+	"interpreter/ast"
+	"interpreter/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `let x = 5;
+let y = 10;
+let foobar = 383838;`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does ont contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		statement := program.Statements[i]
+		if !testLetStatement(t, statement, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, statement ast.Statement, identifier string) bool {
+	if statement.TokenLiteral() != "let" {
+		t.Errorf("statement.TokenLiteral not 'let'. got=%q", statement.TokenLiteral())
+		return false
+	}
+	letStatement, ok := statement.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("statement not *ast.LetStatement. got=%T", statement)
+		return false
+	}
+
+	if letStatement.Name.Value != identifier {
+		t.Errorf("letStatement.Name.Value not '%s'. got=%s", identifier, letStatement.Name.Value)
+		return false
+	}
+
+	if letStatement.Name.TokenLiteral() != identifier {
+		t.Errorf("letStatement.Name.TokenLiteral() not '%s'. got=%s", identifier, letStatement.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
