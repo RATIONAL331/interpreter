ce4b90488065d616599bf36314a16ab7d1bcd19b
김령진 <rational331@gmail.com>
2022-02-14 21:29:35 +0900

    [Chapter 4] 배열을 이용하여 map, reduce 함수 구현해보기

--- main/main.go
+++ main/main.go
@@ -13,6 +13,43 @@ func main() {
 		panic(any(e))
 	}
 
+	/**
+	let map = fn(arr, f) {
+		let iter = fn(arr, accumulated) {
+			if (len(arr) == 0) {
+				accumulated
+			} else {
+				iter(rest(arr), push(accumulated, f(first(arr))));
+			}
+		};
+		iter(arr, []);
+	};
+	let a = [1, 2, 3, 4];
+	let double = fn(x) { x * 2 };
+	map(a, double);
+
+	let map = fn(arr, f) { let iter = fn(arr, accumulated) { if (len(arr) == 0) {accumulated} else {iter(rest(arr), push(accumulated, f(first(arr))));}}; iter(arr, []);}; let a = [1, 2, 3, 4]; let double = fn(x) { x * 2 }; map(a, double);
+	*/
+
+	/**
+	let reduce = fn(arr, initial, f) {
+		let iter = fn(arr, result) {
+			if(len(arr) == 0) {
+				result
+			} else {
+				iter(rest(arr), f(result, first(arr)));
+			}
+		};
+		iter(arr, initial);
+	};
+	let sum = fn(arr) {
+		reduce(arr, 0, fn(initial, el) { initial + el });
+	}
+	sum([1, 2, 3, 4, 5]);
+
+	 let reduce = fn(arr, initial, f) {let iter = fn(arr, result) {if(len(arr) == 0) {result} else {iter(rest(arr), f(result, first(arr)));}};iter(arr, initial);}; let sum = fn(arr) {reduce(arr, 0, fn(initial, el) { initial + el });} sum([1, 2, 3, 4, 5]);
+	*/
+
 	fmt.Printf("Hello %s! This is the Interpreter programming lanuguage!\n", curUser.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
